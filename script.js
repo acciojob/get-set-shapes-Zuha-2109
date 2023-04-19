2af687c259f5594467f240cd551a3bacece8e2cf
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-04-19 15:30:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height){
+        this.width=width;
+        this.height=height;
+    }
+    get getArea(){
+        return (width,height);
+    }
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+    constructor(width,height){
+        super();
+        this.width=width;
+        this.height=height;
+    }
+    getPerimeter(){
+        //perimeter=4*(a*a)
+        let result=4*(width*width);
+        return result;
+    }
+}
+
+let result=new Rectangle(4,5);
+console.log(result.getPerimeter);
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
